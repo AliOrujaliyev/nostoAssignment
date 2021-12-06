6d598c53e3bcb8364910f3e7d80be7edb695343e
AliOrujaliyev <orujaliyevali@gmail.com>
2021-12-06 21:23:45 +0200

    Changed GET to the POST request

--- src/main/java/com/aliorujaliyev/resource/nostoResource.java
+++ src/main/java/com/aliorujaliyev/resource/nostoResource.java
@@ -1,14 +1,16 @@
 package com.aliorujaliyev.resource;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 
 
 @Path("/number")
 public class nostoResource {
 
     @POST
-    @Path("/numberCheck")
-    public String numberCheck() {
-        return "Hello";
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/numberCheck/{number}")
+    public String numberCheck(@PathParam("number") String input){
+        return input;
     }
 }
