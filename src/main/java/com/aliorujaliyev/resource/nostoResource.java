1946cb62b10a59fc6428bac163024afc799da9e9
AliOrujaliyev <orujaliyevali@gmail.com>
2021-12-06 21:27:43 +0200

    Checking input type

--- src/main/java/com/aliorujaliyev/resource/nostoResource.java
+++ src/main/java/com/aliorujaliyev/resource/nostoResource.java
@@ -11,6 +11,22 @@ public class nostoResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("/numberCheck/{number}")
     public String numberCheck(@PathParam("number") String input){
+//        Default return value "No answer" for the unsuccessful case
+        String result = "No answer";
+        long number;
+
+//        Discarding any inputs that are not numeric and less than 0
+//        Long can't be larger than 9223372036854775807, so no need to check number > Long.MAX_VALUE
+        try {
+            number = Long.parseLong(input);
+            if (number < 0){
+                return result;
+            }
+        } catch (NumberFormatException nfe) {
+            return result;
+        }
+
+
         return input;
     }
 }
